86dfb70ab325ad8c3f1b9c58ea1b0cf9d7326a3b
Aayush Dutt <aayushd100@gmail.com>
2021-07-21 00:17:28 +0530

    enable killed villagers to vote (half weight)

--- routes/room.js
+++ routes/room.js
@@ -1,92 +1,141 @@
-const express     = require("express");
-const router      = express.Router();
+const express = require("express");
+const router = express.Router();
 const Room = require("../models/Room");
 const User = require("../models/User");
-const {authenticateUser, roomIsPlaying} = require('../middlewares');
+const { authenticateUser, roomIsPlaying } = require("../middlewares");
 
-router.get('/', authenticateUser, (req, res) => {
-    const {roomId, userId} = req.cookies
-    Room.findById(roomId, (err, roomData) => {
-        if(err) return res.send("Internal Server Error")
-        if(!roomData) return res.send("Room is no longer available")
-        
-        User.find({roomId}, (err, participants) => {
-            let self
-            participants.forEach(participant => {
-                if(participant._id == userId) {self = participant; return;}
-            })
-    
-            return res.render('room', {participants, roomData, self, title: "Assassination Room"})
-        })
-    })
-})
+router.get("/", authenticateUser, (req, res) => {
+  const { roomId, userId } = req.cookies;
+  Room.findById(roomId, (err, roomData) => {
+    if (err) return res.send("Internal Server Error");
+    if (!roomData) return res.send("Room is no longer available");
 
-router.get('/vote/:participantId', authenticateUser, roomIsPlaying, (req, res) => {
-    const {roomId, userId} = req.cookies
-    User.findById(userId, (err, foundUser) => {
-        if(!foundUser || err) return res.send("Couldn't vote, please try again")
-
-        if(foundUser.isKilled || !foundUser.canPlay || foundUser.role !== 'villager') {
-            return res.send("Vote was not allowed")
+    User.find({ roomId }, (err, participants) => {
+      let self;
+      participants.forEach((participant) => {
+        if (participant._id == userId) {
+          self = participant;
+          return;
         }
+      });
 
-        User.findByIdAndUpdate(req.params.participantId, { $inc: {'votesRecieved': 1 } }, (err, updatedParticipant) => {
-            if(err) return res.send("Couldn't vote, internal server error")
-            User.findByIdAndUpdate(userId, {canPlay: false}, (err) => {
-                if(err) return res.send("Couldn't vote, internal server error")
-                return res.redirect('/room')
-            })
-        })
-    })
-})
+      return res.render("room", {
+        participants,
+        roomData,
+        self,
+        title: "Assassination Room",
+      });
+    });
+  });
+});
 
-router.get('/reveal/:participantId', authenticateUser, roomIsPlaying, (req, res) => {
-    const {roomId, userId} = req.cookies
+router.get(
+  "/vote/:participantId",
+  authenticateUser,
+  roomIsPlaying,
+  (req, res) => {
+    const { roomId, userId } = req.cookies;
     User.findById(userId, (err, foundUser) => {
-        if(!foundUser || err) return res.send("Couldn't assassinate, please try again")
+      if (!foundUser || err) return res.send("Couldn't vote, please try again");
 
-        if(foundUser.isKilled || !foundUser.canPlay || foundUser.role !== 'seer') {
-            return res.send("Reveal was not allowed")
+      if (!foundUser.canPlay || foundUser.role !== "villager") {
+        return res.send("Vote was not allowed");
+      }
+
+      const increment = foundUser.isKilled ? 1 : 2;
+
+      User.findByIdAndUpdate(
+        req.params.participantId,
+        { $inc: { votesRecieved: increment } },
+        (err, updatedParticipant) => {
+          if (err) return res.send("Couldn't vote, internal server error");
+          User.findByIdAndUpdate(userId, { canPlay: false }, (err) => {
+            if (err) return res.send("Couldn't vote, internal server error");
+            return res.redirect("/room");
+          });
         }
+      );
+    });
+  }
+);
 
-        User.findByIdAndUpdate(req.params.participantId, { isRevealed: true }, (err, updatedParticipant) => {
-            if(err) return res.send("Couldn't reveal, internal server error")
-            User.findByIdAndUpdate(userId, {canPlay: false}, (err) => {
-                if(err) return res.send("Couldn't reveal, internal server error")
-                return res.redirect('/room')
-            })
-        })
-    })
-})
-
-router.get('/assassinate/:participantId', authenticateUser, roomIsPlaying, (req, res) => {
-    const {roomId, userId} = req.cookies
+router.get(
+  "/reveal/:participantId",
+  authenticateUser,
+  roomIsPlaying,
+  (req, res) => {
+    const { roomId, userId } = req.cookies;
     User.findById(userId, (err, foundUser) => {
-        if(!foundUser || err) return res.send("Couldn't assassinate, please try again")
+      if (!foundUser || err)
+        return res.send("Couldn't assassinate, please try again");
 
-        if(foundUser.isKilled || !foundUser.canPlay || foundUser.role !== 'assassin') {
-            return res.send("Illegal Assassination")
+      if (
+        foundUser.isKilled ||
+        !foundUser.canPlay ||
+        foundUser.role !== "seer"
+      ) {
+        return res.send("Reveal was not allowed");
+      }
+
+      User.findByIdAndUpdate(
+        req.params.participantId,
+        { isRevealed: true },
+        (err, updatedParticipant) => {
+          if (err) return res.send("Couldn't reveal, internal server error");
+          User.findByIdAndUpdate(userId, { canPlay: false }, (err) => {
+            if (err) return res.send("Couldn't reveal, internal server error");
+            return res.redirect("/room");
+          });
         }
+      );
+    });
+  }
+);
 
-        User.findByIdAndUpdate(req.params.participantId, { isAssassinated: true }, (err, updatedParticipant) => {
-            if(err) return res.send("Couldn't assassinate, internal server error")
-            User.findByIdAndUpdate(userId, {canPlay: false}, (err) => {
-                if(err) return res.send("Couldn't assassinate, internal server error")
-                return res.redirect('/room')
-            })
-        })
-    })
-})
+router.get(
+  "/assassinate/:participantId",
+  authenticateUser,
+  roomIsPlaying,
+  (req, res) => {
+    const { roomId, userId } = req.cookies;
+    User.findById(userId, (err, foundUser) => {
+      if (!foundUser || err)
+        return res.send("Couldn't assassinate, please try again");
 
-router.get('/toggleAutoRefresh', authenticateUser, (req, res) => {
-    const {roomId, userId} = req.cookies
-    User.findById(userId, (err,foundUser) => {
-        if(err) return res.send("Couldn't toggle autorefresh")
-        foundUser.autoRefresh = !foundUser.autoRefresh
-        foundUser.save().then(() => {
-            return res.redirect('/room')
-        })
-    })
-})
+      if (
+        foundUser.isKilled ||
+        !foundUser.canPlay ||
+        foundUser.role !== "assassin"
+      ) {
+        return res.send("Illegal Assassination");
+      }
 
-module.exports = router
+      User.findByIdAndUpdate(
+        req.params.participantId,
+        { isAssassinated: true },
+        (err, updatedParticipant) => {
+          if (err)
+            return res.send("Couldn't assassinate, internal server error");
+          User.findByIdAndUpdate(userId, { canPlay: false }, (err) => {
+            if (err)
+              return res.send("Couldn't assassinate, internal server error");
+            return res.redirect("/room");
+          });
+        }
+      );
+    });
+  }
+);
+
+router.get("/toggleAutoRefresh", authenticateUser, (req, res) => {
+  const { roomId, userId } = req.cookies;
+  User.findById(userId, (err, foundUser) => {
+    if (err) return res.send("Couldn't toggle autorefresh");
+    foundUser.autoRefresh = !foundUser.autoRefresh;
+    foundUser.save().then(() => {
+      return res.redirect("/room");
+    });
+  });
+});
+
+module.exports = router;
--- routes/roomAdmin.js
+++ routes/roomAdmin.js
@@ -1,190 +1,221 @@
 const express = require("express");
 const router = express.Router();
-const { authenticateAdmin } = require('../middlewares');
+const { authenticateAdmin } = require("../middlewares");
 const Room = require("../models/Room");
 const User = require("../models/User");
 
 function randomBetween(a, b) {
-    return Math.floor(a + Math.random() * (b - a))
+  return Math.floor(a + Math.random() * (b - a));
 }
 
-router.get('/', authenticateAdmin, (req, res) => {
-    const { roomId } = req.cookies
+router.get("/", authenticateAdmin, (req, res) => {
+  const { roomId } = req.cookies;
 
-    User.find({ roomId: roomId }, (err, participants) => {
-        if (err) return res.send('Internal Server Error')
-        Room.findById(roomId, (err, roomData) => {
-            if (err) return res.send('Internal Server Error')
-            return res.render('roomAdmin', { participants, roomData, title: "Admin Room" })
-        })
-    })
-})
+  User.find({ roomId: roomId }, (err, participants) => {
+    if (err) return res.send("Internal Server Error");
+    Room.findById(roomId, (err, roomData) => {
+      if (err) return res.send("Internal Server Error");
+      return res.render("roomAdmin", {
+        participants,
+        roomData,
+        title: "Admin Room",
+      });
+    });
+  });
+});
 
-router.get('/closeRoom', authenticateAdmin, (req, res) => {
-    const { roomId } = req.cookies
-    Room.findByIdAndUpdate(roomId, { acceptingParticipants: false }, (err, updatedRoom) => {
-        if (err) return res.send("Couldn't toggle room")
-        res.redirect('/roomAdmin')
-    })
-})
+router.get("/closeRoom", authenticateAdmin, (req, res) => {
+  const { roomId } = req.cookies;
+  Room.findByIdAndUpdate(
+    roomId,
+    { acceptingParticipants: false },
+    (err, updatedRoom) => {
+      if (err) return res.send("Couldn't toggle room");
+      res.redirect("/roomAdmin");
+    }
+  );
+});
 
-router.get('/openRoom', authenticateAdmin, (req, res) => {
-    const { roomId } = req.cookies
-    Room.findByIdAndUpdate(roomId, { acceptingParticipants: true }, (err, updatedRoom) => {
-        if (err) return res.send("Couldn't toggle room")
-        res.redirect('/roomAdmin')
-    })
-})
+router.get("/openRoom", authenticateAdmin, (req, res) => {
+  const { roomId } = req.cookies;
+  Room.findByIdAndUpdate(
+    roomId,
+    { acceptingParticipants: true },
+    (err, updatedRoom) => {
+      if (err) return res.send("Couldn't toggle room");
+      res.redirect("/roomAdmin");
+    }
+  );
+});
 
-router.post('/assignRoles', authenticateAdmin, (req, res) => {
-    const { roomId } = req.cookies
-    const { seers, assassins } = req.body
-    if(!seers || !assassins) return res.send("Seers and Assassins required")
+router.post("/assignRoles", authenticateAdmin, (req, res) => {
+  const { roomId } = req.cookies;
+  const { seers, assassins } = req.body;
+  if (!seers || !assassins) return res.send("Seers and Assassins required");
 
-    User.find({ roomId: roomId })
-    .then((participants) => {
-        if(seers + assassins > participants) {
-            return res.send("not enough participants")
+  User.find({ roomId: roomId }).then((participants) => {
+    if (seers + assassins > participants) {
+      return res.send("not enough participants");
+    }
+
+    let queue = [];
+    const len = participants.length;
+    let remAssassins = parseInt(assassins),
+      remSeers = parseInt(seers);
+    for (let i = 0; i < len; i++) {
+      let toSwapIndex = randomBetween(i, len);
+      // perform swap
+      let temp = participants[i];
+      participants[i] = participants[toSwapIndex];
+      participants[toSwapIndex] = temp;
+
+      // assign role
+      if (remAssassins) {
+        participants[i].role = "assassin";
+        queue.push(participants[i].save());
+        remAssassins--;
+      } else if (remSeers) {
+        participants[i].role = "seer";
+        queue.push(participants[i].save());
+        remSeers--;
+      } else {
+        participants[i].role = "villager";
+        queue.push(participants[i].save());
+      }
+    }
+
+    Promise.all(queue)
+      .then(() => {
+        Room.findByIdAndUpdate(
+          roomId,
+          { rolesAssigned: true, status: ["Roles are assigned"] },
+          (err, updatedRoom) => {
+            return res.redirect("/roomAdmin");
+          }
+        );
+      })
+      .catch((err) => {
+        return res.send(err);
+      });
+  });
+});
+
+router.get("/startRound", authenticateAdmin, (req, res) => {
+  const { roomId } = req.cookies;
+  Room.findById(roomId, (err, foundRoom) => {
+    if (err || !foundRoom || !foundRoom.rolesAssigned) return;
+
+    User.updateMany(
+      { roomId, isKilled: false },
+      {
+        votesRecieved: 0,
+        canPlay: true,
+      },
+      (err, updatedParticipants) => {
+        if (err) return res.send("Couldn't start round");
+        Room.updateOne(
+          { _id: roomId },
+          { isPlaying: true, status: [] },
+          (err, updatedRoom) => {
+            if (err) return res.send("Couldn't start round");
+            return res.redirect("/roomAdmin");
+          }
+        );
+      }
+    );
+  });
+});
+
+router.get("/getResults", authenticateAdmin, (req, res) => {
+  const { roomId } = req.cookies;
+  User.find({ roomId }, (err, participants) => {
+    let assassinated = [];
+    let maxVoted;
+    let maxVotes = 0;
+
+    participants.forEach((participant, idx) => {
+      if (!participant.isKilled) {
+        if (participant.isAssassinated) {
+          participant.isKilled = true;
+          participant.canPlay = participant.role === "villager"; // villagers can still play
+          assassinated.push(participant);
         }
-
-        let queue = []
-        const len = participants.length
-        let remAssassins = parseInt(assassins) , remSeers = parseInt(seers)
-        for(let i=0; i<len; i++) {
-            let toSwapIndex =  randomBetween(i, len);
-            // perform swap
-            let temp = participants[i]
-            participants[i] = participants[toSwapIndex]
-            participants[toSwapIndex] = temp
-
-            // assign role
-            if(remAssassins) {
-                participants[i].role = 'assassin'    
-                queue.push(participants[i].save())
-                remAssassins--
-            }
-            else if(remSeers) {
-                participants[i].role = 'seer'    
-                queue.push(participants[i].save())
-                remSeers--
-            }
-            else {
-                participants[i].role = 'villager'    
-                queue.push(participants[i].save())
-            }
+        if (participant.votesRecieved > maxVotes) {
+          maxVoted = participant;
+          maxVotes = participant.votesRecieved;
         }
+      }
+    });
 
-        Promise.all(queue)
-        .then(() => {
-            Room.findByIdAndUpdate(roomId, {rolesAssigned: true, status: ["Roles are assigned"]}, (err, updatedRoom) => {
-                return res.redirect('/roomAdmin')
-            })
-        })
-        .catch((err) => {
-            return res.send(err)
-        })
-    })
-})
+    let status = [];
+    assassinated.forEach((assassinatedPlayer) => {
+      status.push(`${assassinatedPlayer.username} was assassinated`);
+    });
 
-router.get('/startRound', authenticateAdmin, (req, res) => {
-    const { roomId } = req.cookies
-    Room.findById(roomId, (err, foundRoom) => {
-        if (err || !foundRoom || !foundRoom.rolesAssigned)
-        return
-        
-        User.updateMany({ roomId, isKilled: false }, {
-            votesRecieved: 0,
-            canPlay: true
-        }, (err, updatedParticipants) => {
-            if (err) return res.send("Couldn't start round")
-            Room.updateOne({ _id: roomId }, { isPlaying: true, status: [] }, (err, updatedRoom) => {
-                if (err) return res.send("Couldn't start round")
-                return res.redirect('/roomAdmin')
-            })
-        })
-    })
-})
+    if (maxVotes > 0 && maxVoted) {
+      if (!maxVoted.isKilled) {
+        //if not already assassinated
+        status.push(`${maxVoted.username} was killed by villagers`);
+        maxVoted.isKilled = true;
+        maxVoted.canPlay = maxVoted.role === "villager";
+        assassinated.push(maxVoted);
+      } else {
+        status.push(`${maxVoted.username} was killed by villagers as well`);
+      }
+    }
 
-router.get('/getResults', authenticateAdmin, (req, res) => {
-    const { roomId } = req.cookies
-    User.find({roomId}, (err, participants) => {
-        let assassinated = []
-        let maxVoted
-        let maxVotes = 0
-
-        participants.forEach((participant, idx) => {
-            if(!participant.isKilled) {
-                if(participant.isAssassinated) {
-                    participant.isKilled = true
-                    participant.canPlay = false
-                    assassinated.push(participant)
-                }
-                if(participant.votesRecieved > maxVotes) {
-                    maxVoted = participant
-                    maxVotes = participant.votesRecieved
-                }
+    Promise.all(assassinated.map((player) => player.save()))
+      .then(() => {
+        Room.findByIdAndUpdate(
+          roomId,
+          { isPlaying: false, status },
+          (err, updatedRoom) => {
+            if (err) {
+              console.log(err);
+              return res.send("Couldn't update room");
             }
-        })
+            return res.redirect("/roomAdmin");
+          }
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.send("Couldn't get results, internal server error");
+      });
+  });
+});
 
-        let status = []
-        assassinated.forEach(assassinatedPlayer => {
-            status.push(`${assassinatedPlayer.username} was assassinated`)
-        })
-
-        if(maxVotes > 0 && maxVoted) {
-            if(!maxVoted.isKilled) {
-                //if not already assassinated
-                status.push(`${maxVoted.username} was killed by villagers`)
-                maxVoted.isKilled = true
-                maxVoted.canPlay = false
-                assassinated.push(maxVoted)
-            }
-            else {
-                status.push(`${maxVoted.username} was killed by villagers as well`)
-            }
+router.get("/resetRoom", authenticateAdmin, (req, res) => {
+  const { roomId } = req.cookies;
+  Room.findByIdAndUpdate(
+    roomId,
+    {
+      isPlaying: false,
+      rolesAssigned: false,
+      status: ["Room resetted"],
+    },
+    (err, updatedRoom) => {
+      if (err) return res.send("Couldn't reset room");
+      User.updateMany(
+        { roomId },
+        {
+          role: "",
+          votesRecieved: 0,
+          canPlay: false,
+          isKilled: false,
+          isRevealed: false,
+          isAssassinated: false,
+        },
+        (err, participants) => {
+          if (err) {
+            console.log(err);
+            return res.send("Couldn't reset room");
+          }
+          return res.redirect("/roomAdmin");
         }
+      );
+    }
+  );
+});
 
-        Promise.all(assassinated.map(player => player.save()))
-        .then(() => {
-            Room.findByIdAndUpdate(roomId, {isPlaying: false, status}, (err, updatedRoom) => {
-                if(err) {
-                    console.log(err)
-                    return res.send("Couldn't update room")
-                }
-                return res.redirect('/roomAdmin')
-            })
-        })
-        .catch(err => {
-            console.log(err)
-            return res.send("Couldn't get results, internal server error")
-        })
-    })
-})
-
-
-router.get('/resetRoom', authenticateAdmin, (req, res) => {
-    const { roomId } = req.cookies
-    Room.findByIdAndUpdate(roomId, {
-        isPlaying: false,
-        rolesAssigned: false,
-        status: ["Room resetted"]
-    },  (err, updatedRoom) => {
-        if(err) return res.send("Couldn't reset room")
-        User.updateMany({roomId}, {
-            role: "",
-            votesRecieved: 0,
-            canPlay: false,
-            isKilled: false,
-            isRevealed: false,
-            isAssassinated: false
-        }, (err, participants) => {
-            if(err) {
-                console.log(err)
-                return res.send("Couldn't reset room")
-            }
-            return res.redirect('/roomAdmin')
-        }) 
-    })
-})
-
-module.exports = router
+module.exports = router;
